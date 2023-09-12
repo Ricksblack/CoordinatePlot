22e8c7119ecb571a4366905f12ab72529dcf5a86
Ricardo Moreno <rmm_92@outlook.com>
2023-09-12 08:08:19 -0700

    Added description to sliders and function to round

--- app/src/main/java/com/kodeco/android/coordplot/MainActivity.kt
+++ app/src/main/java/com/kodeco/android/coordplot/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
@@ -15,6 +16,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -29,6 +31,9 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kodeco.android.coordplot.ui.theme.MyApplicationTheme
+import java.math.RoundingMode
+import java.text.DecimalFormat
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,6 +51,12 @@ fun PlotSurface() {
     var xPercentage: Float by remember { mutableStateOf(0.5f) }
     var yPercentage: Float by remember { mutableStateOf(0.5f) }
 
+    fun getRounded(value: Float) : String? {
+        val format = DecimalFormat("#.#")
+        format.roundingMode = RoundingMode.DOWN
+        return format.format(value)
+    }
+
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = Color.White
@@ -58,24 +69,55 @@ fun PlotSurface() {
                 xPercent = xPercentage,
                 yPercent = yPercentage
             )
-            Slider(
-                value = xPercentage,
-                valueRange = 0f..1.0f,
-                onValueChange = { xValue ->
-                    xPercentage = xValue
-                    println(xValue)
-                },
-                modifier = Modifier.padding(16.dp)
-            )
-            Slider(
-                value = yPercentage,
-                valueRange = 0f .. 1.0f,
-                onValueChange = { yValue ->
-                    yPercentage = yValue
-                    println(yValue)
-                },
-                modifier = Modifier.padding(16.dp)
-            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+            ) {
+                Text(
+                    text = "X axis: ${getRounded(xPercentage)}",
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .size(
+                            width = 120.dp,
+                            height = 20.dp
+                        )
+                )
+                Slider(
+                    value = xPercentage,
+                    valueRange = 0f..1.0f,
+                    onValueChange = { xValue ->
+                        xPercentage = xValue
+                        println(xValue)
+                    },
+                    modifier = Modifier
+                        .padding(16.dp)
+                )
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+            ) {
+                Text(
+                    text = "Y axis: ${getRounded(yPercentage)}",
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .size(
+                            width = 120.dp,
+                            height = 20.dp
+                        )
+                )
+                Slider(
+                    value = yPercentage,
+                    valueRange = 0f..1.0f,
+                    onValueChange = { yValue ->
+                        yPercentage = yValue
+                        println(yValue)
+                    },
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
         }
     }
 }
