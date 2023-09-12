78aad2f929d208e4688d3490468602d3b621acb0
Ricardo Moreno <rmm_92@outlook.com>
2023-09-12 08:30:23 -0700

    Updated colors in Color.kt and updated in Theme.kt

--- app/src/main/java/com/kodeco/android/coordplot/ui/theme/Color.kt
+++ app/src/main/java/com/kodeco/android/coordplot/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.kodeco.android.coordplot.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple90 = Color(0xFFD0BCFF)
+val PurpleGrey90 = Color(0xFFCCC2EC)
+val Pink90 = Color(0xFFEFB8C9)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple30 = Color(0xFF6650a3)
+val PurpleGrey30 = Color(0xFF625b72)
+val Pink30 = Color(0xFF7D5261)
