1fd2b1aeac3a01680ff39a295bb1aea90ac615b0
Ricardo Moreno <rmm_92@outlook.com>
2023-09-13 12:16:39 -0700

    Added new theme for the splash screen

--- app/src/main/java/com/kodeco/android/coordplot/MainActivity.kt
+++ app/src/main/java/com/kodeco/android/coordplot/MainActivity.kt
@@ -3,11 +3,13 @@ package com.kodeco.android.coordplot
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.kodeco.android.coordplot.components.PlotSurface
 import com.kodeco.android.coordplot.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
