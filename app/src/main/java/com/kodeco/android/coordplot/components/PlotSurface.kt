241d408381fbbf1b2c9d3eaae6cb46e1cd0090c9
Ricardo Moreno <rmm_92@outlook.com>
2023-09-15 01:29:21 -0700

    Added handling for portrait mode

--- app/src/main/java/com/kodeco/android/coordplot/components/PlotSurface.kt
+++ app/src/main/java/com/kodeco/android/coordplot/components/PlotSurface.kt
@@ -1,7 +1,9 @@
 package com.kodeco.android.coordplot.components
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -14,6 +16,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import com.kodeco.android.coordplot.ui.theme.MyApplicationTheme
 
@@ -21,26 +24,63 @@ import com.kodeco.android.coordplot.ui.theme.MyApplicationTheme
 fun PlotSurface() {
     var xPercentage: Float by rememberSaveable { mutableFloatStateOf(0.5f) }
     var yPercentage: Float by rememberSaveable { mutableFloatStateOf(0.5f) }
+    var isPortrait = false
 
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = Color.White
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier
-                .verticalScroll(rememberScrollState())
-        ) {
-            Map(
-                xPercent = xPercentage,
-                yPercent = yPercentage
-            )
-            TaggedSlider(title = "X axis: ", percentage = xPercentage) { value ->
-                xPercentage = value
+        when (LocalConfiguration.current.orientation) {
+            Configuration.ORIENTATION_PORTRAIT -> isPortrait = true
+            Configuration.ORIENTATION_LANDSCAPE -> isPortrait = false
+        }
+        if (isPortrait) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+            ) {
+                Map(
+                    xPercent = xPercentage,
+                    yPercent = yPercentage
+                )
+                TaggedSlider(
+                    title = "X axis: ",
+                    percentage = xPercentage
+                ) { value ->
+                    xPercentage = value
+                }
+                TaggedSlider(
+                    title = "Y axis: ",
+                    percentage = yPercentage
+                ) { value ->
+                    yPercentage = value
+                }
             }
-            TaggedSlider(title = "Y axis: ", percentage = yPercentage) { value ->
-                yPercentage = value
+        } else {
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Map(
+                    xPercent = xPercentage,
+                    yPercent = yPercentage
+                )
+                Column {
+                    TaggedSlider(
+                        title = "X axis: ",
+                        percentage = xPercentage
+                    ) { value ->
+                        xPercentage = value
+                    }
+                    TaggedSlider(
+                        title = "Y axis: ",
+                        percentage = yPercentage
+                    ) { value ->
+                        yPercentage = value
+                    }
+                }
             }
         }
     }
