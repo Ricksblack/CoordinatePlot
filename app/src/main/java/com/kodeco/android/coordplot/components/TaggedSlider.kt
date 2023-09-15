ff3073674fb0d4cef7bb1a9f44ebef26046a3620
Ricardo Moreno <rmm_92@outlook.com>
2023-09-15 01:39:16 -0700

    Added preview for TaggedSlider

--- app/src/main/java/com/kodeco/android/coordplot/components/TaggedSlider.kt
+++ app/src/main/java/com/kodeco/android/coordplot/components/TaggedSlider.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import java.math.RoundingMode
 import java.text.DecimalFormat
@@ -45,4 +46,10 @@ fun TaggedSlider(
                 .padding(16.dp)
         )
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TaggedSliderPreview() {
+    TaggedSlider(title = "Title", percentage = 1.0f, onValueChanged = {} )
 }
